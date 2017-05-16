df4d1292a7ff7257cac9b4c8c851c724213709b8
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2017-05-16 16:50:42 +0900

    Initial Commit

---
+++ BMI　APPTests/BMI_APPTests.swift
@@ -0,0 +1,36 @@
+//
+//  BMI_APPTests.swift
+//  BMI　APPTests
+//
+//  Created by 渡邉拓未 on 2017/05/16.
+//  Copyright © 2017年 takumi watanabe. All rights reserved.
+//
+
+import XCTest
+@testable import BMI_APP
+
+class BMI_APPTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
