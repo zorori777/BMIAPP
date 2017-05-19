8b9082eba9813bfcd3c27e6314d081a8e3643e46
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2017-05-19 12:09:49 +0900

    Initial commit

--- BMI　APP/ViewController.swift
+++ BMI　APP/ViewController.swift
@@ -8,18 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate  {
+    @IBOutlet weak var youtall: UITextField!
+    @IBOutlet weak var youweight: UITextField!
+    @IBOutlet weak var myage: UITextField!
+    @IBOutlet weak var yougender: UISegmentedControl!
+    
+    var finalvalue :Double!
+    var realgender :Int!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        youtall.delegate = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "pushsegue"{
+            let secondViewController = segue.destination as! ViewController2ViewController
+            finalvalue = caluculate()
+            realgender = yougender.selectedSegmentIndex
 
+            secondViewController.params = finalvalue
+            secondViewController.ageparams = self.myage.text
+            secondViewController.genderparams = yougender.titleForSegment(at: realgender)
+        }
+    }
+    
+    @IBAction func BMIbutton(_ sender: UIButton) {
+    }
+    
+    func caluculate() -> Double {
+        let m_tall :Double
+        let tall = Double(youtall.text!)
+         m_tall = tall! / 100
+        let weight = Double(youweight.text!)
+        let realweight = weight!
+        let dbtall = m_tall * m_tall
+        
+        var result = realweight / dbtall * 10
+        return (round(result) / 10)
+        //http://swift.tecc0.com/?p=476
 
+    }
+
+    func textFieldShouldReturn(_ youtall: UITextField) -> Bool {
+        youtall.resignFirstResponder()
+        return true
+    }
 }
-
---
+++ ViewController2ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController2ViewController.swift
+//  BMI　APP
+//
+//  Created by 渡邉拓未 on 2017/05/16.
+//  Copyright © 2017年 takumi watanabe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2ViewController: UIViewController {
+    @IBOutlet weak var finalresult: UILabel!
+    @IBOutlet weak var finalgender: UILabel!
+    @IBOutlet weak var finalage: UILabel!
+    
+    var params: Double!
+    var ageparams: String!
+    var genderparams: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let final: String = String(params)
+//        let finalgen: String = String(genderparams)
+//        print(finalgen)
+        self.finalresult.text = final
+        self.finalage.text = self.ageparams
+        self.finalgender.text = self.genderparams
+        
+     
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
